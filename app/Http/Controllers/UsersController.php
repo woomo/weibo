181e5398ecd294d154a821240069ab5ebc417608
vagrant <vagrant@homestead>
2021-08-07 11:07:20 +0000

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,15 @@ class UsersController extends Controller
 
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        var_dump($request->post());
+        $this->validate($request,[
+            'name'=>'required|unique:users|max:50|min:4',
+            'email'=>'required|email|unique:users|max:50',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        
+    }
 }
